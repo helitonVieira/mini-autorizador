bea995f67b2c3bdc9fca9c4735ffb2d3a59aab60
heliton Vieira <helitondba@gmail.com>
2022-06-13 20:01:14 -0300

    criacao do projeto

---
+++ src/test/java/com/heliton/MiniAutorizadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heliton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniAutorizadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
